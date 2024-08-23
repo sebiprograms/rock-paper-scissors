82e103ae430d1c19ebb93f9b57ff9863ac85abae
Sebastian Stockton <96149632+sebiprograms@users.noreply.github.com>
2024-08-23 15:53:13 -0500

    playRound loop added
    Adds a playRound loop inside playGame so you can
    play 5 rounds to see who the ultimate champion is!
    Nest

--- index.js
+++ index.js
@@ -7,26 +7,38 @@ function getHumanChoice() {
     return parseInt(prompt("Input 1: Rock,  2: Paper, 3:  Scissors", "Input number"));
 }
 
-const humanChoice = getHumanChoice()
-const computerChoice = getComputerChoice();
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-
+    if (humanChoice == computerChoice){
+        console.log("Tie!")
+    } else if ((humanChoice == 1 && computerChoice == 2) || (humanChoice == 2 && computerChoice == 3) || (humanChoice == 3 && computerChoice == 1)) {
+        console.log("Computer Wins!")
+        computerScore += 1;
+    } else {
+        console.log("Human Wins!");
+        humanScore += 1;
+    }
 }
 
 
 // Main Game loop
 function playGame {
+    let gameOn = true;
+    
 
+    while(gameOn == true) {
 
-
-
-
-
-
-
+        for (let i = 0; i <= 5; i++) {
+            const humanChoice = getHumanChoice()
+            const computerChoice = getComputerChoice();
+            playRound(humanChoice, computerChoice);
+        }
+        //ends game after 5 rounds
+        console.log("The Champion")
+        gameOn = false;
+    }
 
 
 }
