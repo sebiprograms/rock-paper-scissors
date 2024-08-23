c2f176fc0ca0547a47c16d39df25e0166e776fa2
Sebastian Stockton <96149632+sebiprograms@users.noreply.github.com>
2024-08-23 15:13:55 -0500

    getHuman choice function created
    Adds a function to get players choice. And also
    a function to create a game loop.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function getComputerChoice() {
     return Math.floor(Math.random() * (4 - 1))+1;
 }
 
+function getHumanChoice() {
+    return parseInt(prompt("Input 1: Rock,  2: Paper, 3:  Scissors", "Input number"));
+}
+
+let humanScore = 0;
+let computerScore = 0;
+const humanChoice = getHumanChoice()
+const computerChoice = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+
+}
